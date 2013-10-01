783cdf8488badc29fa0f781cfdb19236637a87e5
Russell Haering <russell.haering@rackspace.com>
2013-09-30 17:35:32 -0700

    bump to v0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txetcd',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'treq >= 0.2',
